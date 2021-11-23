428aa25501bf463f9f5ddfcaca624af11fd26208
csslick <digitone@naver.com>
2021-11-23 22:39:18 +0900

    add jquery

--- src/index.js
+++ src/index.js
@@ -1,5 +1,8 @@
 import sub from './sub.js';
 import css from './style.css';
+import $ from 'jquery';
 
 console.log('hello webpack!');
 document.getElementById('root').innerHTML = sub();
+
+$('body').append('<p>hello jquery</p>');
